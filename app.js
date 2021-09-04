6dd2e7f6719441ff6cdfeed3019196ea3aa793dd
notPesol <pesol2012@hotmail.com>
2021-09-04 16:34:43 +0700

    create register page

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 const expressSession = require('express-session');
 app.use(expressSession({
   secret: process.env.SESSION_SECRET || 'secretMustSecret',
--- routes/member.js
+++ routes/member.js
@@ -1,6 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+const Member = require('../Models/Member');
 
+const bcrypt = require('bcrypt');
+
+router.get('/login', (req, res, next) => {
+  res.render('member/login');
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const member = await Member.findOne({ email });
+    if (member) {
+      const isValid = bcrypt.compareSync(password, member.password);
+      if (isValid) {
+        req.session.user = member;
+        return res.redirect('/');
+      }
+
+      // if not true
+      res.redirect('/member/login');
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/register', (req, res, next) => {
+  res.render('member/register');
+});
+
+router.get('/logout', (req, res, next) => {
+  delete req.session.user;
+  res.redirect('/');
+});
 
 module.exports = router;
